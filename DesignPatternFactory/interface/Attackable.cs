8cd76bea996dd3b6a1496b3eab9f6430b607b6c9
HieuTrann18 <hieu.tt.2455@aptechlearning.edu.vn>
2025-03-25 11:23:24 +0700

    Add files via upload

---
+++ DesignPatternFactory/interface/Attackable.cs
@@ -0,0 +1,11 @@
+﻿namespace GameProject.Interfaces
+{
+    public interface IAttackable
+    {
+        void Attack(IAttackable target);
+        int GetHealth();
+        bool IsAlive();
+        void LevelUp();
+        void TakeDamage(int attackPower);
+    }
+}
